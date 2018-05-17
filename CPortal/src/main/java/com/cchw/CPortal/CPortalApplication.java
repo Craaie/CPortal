39159ca7b14dec5cd5420d87d0ab9df92b7c929a
luyang <graaie@163.com>
2018-05-17 20:09:51 +0800

    init commit

---
+++ CPortal/src/main/java/com/cchw/CPortal/CPortalApplication.java
@@ -0,0 +1,12 @@
+package com.cchw.CPortal;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class CPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CPortalApplication.class, args);
+	}
+}
---
+++ CPortal/src/test/java/com/cchw/CPortal/Test.java
@@ -0,0 +1,8 @@
+package com.cchw.CPortal;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(CPortalApplicationTests.A);
+	}
+}
